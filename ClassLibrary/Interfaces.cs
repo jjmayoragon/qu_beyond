ac312418169b5d791670f51b70a257fc3cda41d1
jjmayoragon <68620742+jjmayoragon@users.noreply.github.com>
2021-10-11 09:39:34 -0300

    SetHighPerformanceFashionProject
    Define WPF application as a initial project and set the interface in a library.

---
+++ ClassLibrary/Interfaces.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class Interfaces
+    {
+        public class WordFinder
+        {
+            private readonly IEnumerable<string> _matrix;
+            private readonly IEnumerable<string> _result;
+
+            public WordFinder(IEnumerable<string> matrix)
+            {
+                _matrix = matrix;
+            }
+
+            public IEnumerable<string> Find(IEnumerable<string> wordstream)
+            {
+                //Compare
+
+                //Do the job
+
+                //Result
+                return _result;
+            }
+
+
+        }
+    }
+}
