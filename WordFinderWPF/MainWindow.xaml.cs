090684c3af8fcfd752e2bc2412a1fb1a4b174ca5
jjmayoragon <68620742+jjmayoragon@users.noreply.github.com>
2021-10-28 14:21:41 -0300

    ChangesToInterface
    Some changes to the interface before start coding the logic.

--- WordFinderWPF/MainWindow.xaml.cs
+++ WordFinderWPF/MainWindow.xaml.cs
@@ -20,9 +20,12 @@ namespace WordFinderWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<matrix> mt = new List<matrix>();
+
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void sliderSizeMatrix_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -30,9 +33,39 @@ namespace WordFinderWPF
             
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
+            mt.Add(new matrix { x_pos = 0, y_pos = 1, letter = "h" });
+            mt.Add(new matrix { x_pos = 0, y_pos = 1, letter = "t" });
+            mt.Add(new matrix { x_pos = 4, y_pos = 5, letter = "z" });
+            mt.Add(new matrix { x_pos = 7, y_pos = 2, letter = "k" });
 
+            foreach (var m in mt)
+            {
+                MessageBox.Show(m.y_pos.ToString());
+                MessageBox.Show(m.x_pos.ToString());
+                MessageBox.Show(m.letter.ToString());
+            }
+
+        }
+
+        class matrix
+        {
+            string string Name;
+            public void Presentarse(string h)
+            {
+                Console.WriteLine("Hola {0}, segundo{1}", h, Name);
+            }
+            public static matrix Parsero(string str)
+            {
+                var m = new matrix();
+                m.x_pos = 10;
+                m.y_pos = 2;
+                m.letter = "g";
+            }
+            public int x_pos { get; set; }
+            public int y_pos { get; set; }
+            public string letter { get; set; }
         }
     }
 }
